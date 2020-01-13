c44bc4236d976eb3415f4cfe1e7b6ea62530a03d
hshanx <ms915818993@163.com>
2020-01-13 21:25:04 +0800

    获取bean 中文注释

--- spring-beans/src/main/java/org/springframework/beans/factory/config/BeanPostProcessor.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/BeanPostProcessor.java
@@ -20,6 +20,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.lang.Nullable;
 
 /**
+ * <p>
+ *     在 Bean 完成实例化后，如果我们需要对其进行一些配置、增加一些自己的处理逻辑，那么请使用 BeanPostProcessor。
+ * </p>
  * Factory hook that allows for custom modification of new bean instances &mdash;
  * for example, checking for marker interfaces or wrapping beans with proxies.
  *
