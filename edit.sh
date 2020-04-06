22e1e9ef2a54c049e75cfb257102e041ef3b3314
hshanx <ms915818993@163.com>
2020-04-06 09:53:22 +0800

    update

--- edit.sh
+++ edit.sh
@@ -2,7 +2,7 @@
 #
  
 git filter-branch --force --env-filter '
-    if [ "$GIT_COMMITTER_NAME" = "hanshandaozhang" ];
+    if [ "$GIT_COMMITTER_NAME" = "meisen" ];
     then
         GIT_COMMITTER_NAME="hshanx";
         GIT_COMMITTER_EMAIL="ms915818993@163.com";
